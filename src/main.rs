de9d09030658810fed5068652f300a05fbfa3dd2
Ning <ningxyw@gmail.com>
2020-01-11 21:12:03 +0800

    Responsive web

--- src/main.rs
+++ src/main.rs
@@ -246,35 +246,47 @@ fn main() {
     let silent = matches.is_present("silent");
 
     if !silent {
-        printer.println_out(
-            r#"     Index: {}, Upload: {}, Cache: {}, Cors: {}, Range: {}, Sort: {}, Threads: {}
+        printer
+            .println_out(
+                r#"     Index: {}, Upload: {}, Cache: {}, Cors: {}, Range: {}, Sort: {}, Threads: {}
           Auth: {}, Compression: {}
          https: {}, Cert: {}, Cert-Password: {}
           Root: {},
     TryFile404: {}
        Address: {}
     ======== [{}] ========"#,
-            &vec![
-                enable_string(index),
-                enable_string(upload),
-                enable_string(cache),
-                enable_string(cors),
-                enable_string(range),
-                enable_string(sort),
-                threads.to_string(),
-                auth.unwrap_or("disabled").to_string(),
-                compression_string,
-                (if cert.is_some() { "enabled" } else { "disabled" }).to_string(),
-                cert.unwrap_or("").to_owned(),
-                certpass.unwrap_or("").to_owned(),
-                root.to_str().unwrap().to_owned(),
-                try_file_404.unwrap_or("").to_owned(),
-                format!("{}://{}", if cert.is_some() {"https"} else {"http"}, addr),
-                now_string()
-            ].iter()
+                &vec![
+                    enable_string(index),
+                    enable_string(upload),
+                    enable_string(cache),
+                    enable_string(cors),
+                    enable_string(range),
+                    enable_string(sort),
+                    threads.to_string(),
+                    auth.unwrap_or("disabled").to_string(),
+                    compression_string,
+                    (if cert.is_some() {
+                        "enabled"
+                    } else {
+                        "disabled"
+                    })
+                    .to_string(),
+                    cert.unwrap_or("").to_owned(),
+                    certpass.unwrap_or("").to_owned(),
+                    root.to_str().unwrap().to_owned(),
+                    try_file_404.unwrap_or("").to_owned(),
+                    format!(
+                        "{}://{}",
+                        if cert.is_some() { "https" } else { "http" },
+                        addr
+                    ),
+                    now_string(),
+                ]
+                .iter()
                 .map(|s| (s.as_str(), &color_blue))
-                .collect::<Vec<(&str, &Option<ColorSpec>)>>()
-        ).unwrap();
+                .collect::<Vec<(&str, &Option<ColorSpec>)>>(),
+            )
+            .unwrap();
     }
 
     let mut chain = Chain::new(MainHandler {
@@ -693,7 +705,8 @@ impl MainHandler {
   <input type="submit" value="Upload" />
 </form>
 "#,
-                path=encode_link_path(path_prefix))
+                path = encode_link_path(path_prefix)
+            )
         } else {
             "".to_owned()
         };
@@ -704,6 +717,7 @@ impl MainHandler {
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
   <style> a {{ text-decoration:none; }} </style>
 </head>
 <body>
